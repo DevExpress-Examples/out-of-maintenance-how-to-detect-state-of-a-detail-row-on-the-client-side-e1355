5e67bcb42a58f717f0030a1b212c788086c188df
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,24 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections
+
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub grid_DataBound(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsPostBack) Then
+			grid.DetailRows.ExpandRow(0)
+			grid.DetailRows.ExpandRow(2)
+		End If
+	End Sub
+	Protected Sub grid_CustomJSProperties(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewClientJSPropertiesEventArgs)
+		Dim table As New Hashtable(grid.DetailRows.VisibleCount)
+		For i As Integer = 0 To grid.VisibleRowCount - 1
+			If grid.DetailRows.IsVisible(i) Then
+				table(i) = True
+			End If
+		Next i
+		e.Properties("cpVisibleDetails") = table
+	End Sub
+End Class
